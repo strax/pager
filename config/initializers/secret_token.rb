9a1a54929640a9910bfd2f9bd08b756465a0e45a
Sami Kukkonen <sami.matias.kukkonen@gmail.com>
2013-03-14 19:27:33 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pager::Application.config.secret_token = 'fe8bc429f36e8a33f2c27e9dcb0f26ada183e67c3d1c6495b77917572985f0bffdd438cdc52b4cd2d8d5bb4a5afac1d8202f9c61a29ca7c0a27b11b6a303d079'
